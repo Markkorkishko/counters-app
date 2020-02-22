ae0afbce1cd9be096ff817ac51a50c88bda49a6a
mykorkishko <markkorkishko@gmail.com>
2020-02-22 12:34:54 -0500

    Updates

---
+++ precache-manifest.5bf71a8558434dfaa9396b3849238eea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33b9ae48a9d17d1ddae065874e65388e",
+    "url": "/counters-app/index.html"
+  },
+  {
+    "revision": "47040fb9f7b99eb52feb",
+    "url": "/counters-app/static/css/main.2e650f5b.chunk.css"
+  },
+  {
+    "revision": "fcd5fab01c4f6da6199f",
+    "url": "/counters-app/static/js/2.5abea958.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/counters-app/static/js/2.5abea958.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47040fb9f7b99eb52feb",
+    "url": "/counters-app/static/js/main.457035d7.chunk.js"
+  },
+  {
+    "revision": "5d34e65acea4bc89655f",
+    "url": "/counters-app/static/js/runtime-main.a9824170.js"
+  }
+]);
